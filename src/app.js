bd1aa930d75eb0d76c8a1ac42042b9e0ee90a9e1
Cengizcpr <copurcengizhan@gmail.com>
2019-12-14 15:51:31 +0300

    socket i.o added

--- src/app.js
+++ src/app.js
@@ -12,11 +12,11 @@ mongoose.connect('mongodb://localhost/crud-mongo')
 
 // importing routes
 const indexRoutes = require('./routes/index');
-
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 // middlewares
 app.use(morgan('dev'));
@@ -24,7 +24,28 @@ app.use(express.urlencoded({extended: false}))
 // routes
 app.use('/', indexRoutes);
 
-app.listen(app.get('port'), () => {
+server=app.listen(app.get('port'), () => {
   console.log(`server on port ${app.get('port')}`);
 });
 
+const io = require("socket.io")(server) //socket io implement edilir ve server
+
+io.on('connection', (socket) => {
+    console.log("New user connected")
+
+    //default username
+    socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on("change_username", (data) => {
+        console.log(socket.username + " changed name as " + data.username)
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on("new_message", (data)=>{
+        console.log(socket.username + " is said " + data.message)
+        //broadcast the new message
+        io.sockets.emit("new_message", {message : data.message, username : socket.username})
+    })
+})
