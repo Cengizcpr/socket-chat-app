c35c0c86093b0aa39c9ac355b13c78e605cf8a60
Cengizcpr <copurcengizhan@gmail.com>
2019-12-14 13:21:38 +0300

    message pages added controller fixed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/user');
+let emails;
+
 router.get('/add', async (req, res) => {
   res.render('index');
 });
@@ -23,25 +25,37 @@ router.post('/add', async (req, res, next) => {
 
 
 
+
 router.get('/', async (req, res) => {
-   res.render('edit');
+  res.render('edit');
 });
 
 router.post('/login', async (req, res) => {
  User.findOne({email: req.body.email}).then(resa=>{
     if(resa){
-      res.send({
+       res.send({
         error: true, 
         message: resa.email
-         });}
+         });
+        
+    }
     else{ res.send({
       error: false, 
       message: "Kullacı adı veya şifre yanlış"
        });}
+        emails=resa.email 
+      
+      
 })
-  
+
 });
 
+router.get('/message', async (req, res) => {
+  
+  res.render('message', {
+    user:emails
+  })
+});
 
 
 module.exports = router;
